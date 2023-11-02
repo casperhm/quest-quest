ef72e03a2f8159e1da61cc12d4973afe26b6f092
Matt Magoffin <git+matt@msqr.us>
2023-11-02 15:30:44 +1300

    Fix asset loading.

--- src/cave_scene.ts
+++ src/cave_scene.ts
@@ -5,22 +5,31 @@ export const menuSceneKey = "MenuScene";
 export function cave():
     | Phaser.Types.Scenes.SettingsConfig
     | Phaser.Types.Scenes.CreateSceneFromObjectConfig {
-
-
     return {
-
         preload() {
-            this.load.image('walls', 'backrounds/walls.png');
-            this.load.spritesheet('backround', 'backrounds/backround.png',
+            // NOTE: assets are stored in the public/ folder; reference them with a leading / in source, e.g.
+            //
+            //       this.load.image("walls", "/img/backrounds/walls.png");
+            this.load.image("walls", "/img/backrounds/walls.png");
+
+            // REF: https://newdocs.phaser.io/docs/3.60.0/Phaser.Loader.LoaderPlugin#spritesheet
+            this.load.spritesheet(
+                "backround",
+                "/img/backrounds/backround.png",
                 { frameWidth: 300, frameHeight: 168 }
             );
-            this.load.spritesheet('sloth', 'sloth/spritesheet.png')
+
+            // NOTE: Spritesheet only for identially-sized images; Tilemap CSV might also be
+            //       Atlas JSON might be what you want to use here, e.g.
+            //
+            //      this.load.atlas("sloth", "/img/sloth/spritesheet.png", "/img/sloth/spritesheet.json");
+            //
+            // REF: https://newdocs.phaser.io/docs/3.60.0/Phaser.Loader.LoaderPlugin#atlas
+            // FIXME: this.load.spritesheet("sloth", "/img/sloth/spritesheet.png");
         },
         create() {
-            this.add.image(400, 300, 'walls');
+            this.add.image(400, 300, "walls");
         },
-        update() {
-
-        },
+        update() {},
     };
 }
