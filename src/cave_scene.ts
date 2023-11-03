e926ed744bcf5a0e0939534fc9127ed86d2f2d26
Matt Magoffin <git+matt@msqr.us>
2023-11-03 13:15:34 +1300

    Use ?. to access keyboard because it might not be available.

--- src/cave_scene.ts
+++ src/cave_scene.ts
@@ -1,10 +1,8 @@
 import "phaser";
-import * as Globals from './globals';
+import * as Globals from "./globals";
 export const menuSceneKey = "MenuScene";
 
 let wall: Phaser.GameObjects.Image;
-
-
 
 export function cave():
     | Phaser.Types.Scenes.SettingsConfig
@@ -21,29 +19,38 @@ export function cave():
             // NOTE: Spritesheet only for identially-sized images; Tilemap CSV might also be
             //       Atlas JSON might be what you want to use here, e.g.
             //
-            this.load.atlas("sloth", "/img/sloth/spritesheet.png", "/img/sloth/spritesheet.json");
+            this.load.atlas(
+                "sloth",
+                "/img/sloth/spritesheet.png",
+                "/img/sloth/spritesheet.json"
+            );
             //
             // REF: https://newdocs.phaser.io/docs/3.60.0/Phaser.Loader.LoaderPlugin#atlas
         },
         create() {
             //strech without distortion to fit screen
-            this.scale.displaySize.setAspectRatio(Globals.WIDTH / Globals.HEIGHT);
+            this.scale.displaySize.setAspectRatio(
+                Globals.WIDTH / Globals.HEIGHT
+            );
             this.scale.refresh();
 
             //cave walls/floors
-            wall = this.add.image(Globals.WIDTH / 2, Globals.HEIGHT / 2, "walls");
+            wall = this.add.image(
+                Globals.WIDTH / 2,
+                Globals.HEIGHT / 2,
+                "walls"
+            );
 
             //player drawing from atlas
-            this.add.image(30, 105, 'sloth', 'jump1');
-
-
+            this.add.image(30, 105, "sloth", "jump1");
         },
-
 
         update() {
             //detects keyboard inputs
-            let cursors = this.input.keyboard.createCursorKeys();
-
+            let cursors = this.input.keyboard?.createCursorKeys();
+            if (cursors) {
+                // do stuff with cursors here
+            }
         },
     };
 }
